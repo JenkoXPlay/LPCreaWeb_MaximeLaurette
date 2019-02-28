3e102b6f07e9b5ffe06e418db7baaadb70a304e2
Maxime <maxime.lefebvre-isc.france@ibm.com>
2019-02-28 11:59:52 +0100

    REFAIRE LES FICHIERS CLASS NORMALES
    Créer les fichier name.class.php et VERIFIER LES NOMS DES VARIABLES

--- class/TagPhotoManager.class.php
+++ class/TagPhotoManager.class.php
@@ -35,16 +35,6 @@
             $req->execute();
         }
 
-        // Compter le nbr de tag
-        public function count() {
-            $req = $this->getBd()->prepare("SELECT COUNT(id) AS countid FROM tag_photo");
-            $req->execute();
-            for($data = $req->fetch()){
-                $rep = $data['countid']."tag(s)";
-                return $rep;
-            }
-        }
-
         // tous les tags
         public function getList() {
             $req = $this->getBd()->prepare("SELECT * FROM tag_photo");
@@ -54,10 +44,6 @@
                 $text .= "<b>#".$data['id']."</b>";
                 $text .= " - ".$data['id_tag'];
                 $text .= " - <b>".$data['id_photo']."</b>";
-                /*$text .= " - <i>".$data['password']."</i>";
-                $text .= " - ".if($data['administrator'] == 0){."admin : non".}else{."admin : oui".};
-                $text .= " - ".date("d-m-Y");
-                $text .= " - ".if($data['actif'] == 0){."actif : non".}else{."actif : oui".};*/
                 return $text;
             }
         }
@@ -71,36 +57,9 @@
                 $text .= "<b>#".$data['id']."</b>";
                 $text .= " - ".$data['id_tag'];
                 $text .= " - <b>".$data['id_photo']."</b>";
-                /*$text .= " - <i>".$data['password']."</i>";
-                $text .= " - ".if($data['administrator'] == 0){."admin : non".}else{."admin : oui".};
-                $text .= " - ".date("d-m-Y");
-                $text .= " - ".if($data['actif'] == 0){."actif : non".}else{."actif : oui".};*/
                 return $text;
             }
         }
-
-        /* vérifier si un user exist
-        public function exist($pseudo) {
-            $req = $this->getBd()->prepare("SELECT pseudo FROM user WHERE pseudo='$pseudo'");
-            $rep = $rep->execute();
-            if($rep){
-                return $text = "L'utilisateur existe déjà !";
-            }else{
-                return $text = "L'utilisateur n'existe pas encore !";
-            }
-        }*/
-
-        /* Edit d'un user
-        public function edit(User $user, $id) {
-            $req = $this->getBd()->prepare("UPDATE user SET 'email'=':email',
-                                                            'pseudo'=':pseudo',
-                                                            'mdp'=':mdp'
-                                                         WHERE id='$id'");
-            $req->bindValue(':email', $user->getEmail());
-            $req->bindValue(':pseudo', $user->getPseudo());
-            $req->bindValue(':mdp', $user->getMdp());
-            $req->execute();
-        }*/
 
     }
 
